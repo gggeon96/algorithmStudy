52e7c565faaf7b35de3178b12aea6cd052845433
geon1120 <geon1120@gmail.com>
2022-08-26 17:56:48 +0900

    0826 algorithm update

--- backjoon/BJ_1753_최단경로_김건현.java
+++ backjoon/BJ_1753_최단경로_김건현.java
@@ -1,36 +1,27 @@
-package com.kim.backjoon;
+package day_0824;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+
 public class BJ_1753_최단경로_김건현 {
-    static int INF = 100000000;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static final int INF = (int)1e9;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
         int start = Integer.parseInt(br.readLine()) - 1;
-        int[][] arr = new int[V][V];
-        boolean[] visited = new boolean[V];
-        int[] d = new int[V];
-
-        //d초기화
-        for (int i = 0; i < V; i++) {
-            d[i] = INF;
+        ArrayList<ArrayList<Node>> graph = new ArrayList<>(); 
+        for(int i=0;i<V;i++) {
+        	graph.add(new ArrayList<Node>());
         }
-        d[start] = 0;
-
-        //arr초기화
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++) {
-                if (i == j) arr[i][j] = 0;
-                arr[i][j] = INF;
-            }
-        }
-
         //간선 초기화
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
@@ -38,49 +29,59 @@ public class BJ_1753_최단경로_김건현 {
             int v = Integer.parseInt(st.nextToken()) - 1;
             int w = Integer.parseInt(st.nextToken());
             //같은 간선이라도 더 작으면 갱신
-            if (arr[u][v] > w) {
-                arr[u][v] = w;
-            }
+            
+            //u에서 v로 가는데 비용은 w
+            graph.get(u).add(new Node(v,w));
         }
-
-//        PriorityQueue<Integer> pq = new
+        
+        boolean[] visited = new boolean[V];
+        int[] d = new int[V];
+        Arrays.fill(d, INF);
+        d[start] = 0;
+        
         //로직
-        int min, minVertex;
-        for (int i = 0; i < V; i++) {
-            //step1. 미방문 정점 중 출발지에서 자신으로의 비용이 최소인 정점 선택
-            // (방문해야하는 나머지 정점 중 출발지에서 가장 가까운 정점 찾기)
-            min = INF;
-            minVertex = 0;
-            for (int j = 0; j < V; j++) {
-                if (!visited[j] && (min > d[j])) {
-                    min = d[j];
-                    minVertex = j;
-                }
-            }
-
-
-
-            //step2. 방문쳌
-//            for (int k = 0; k < V; k++) {
-//                if (d[k] == INF) System.out.print("INF ");
-//                else System.out.print(d[k]+" ");
-//            }
-//            System.out.println();
-            visited[minVertex] = true;
-
-            //step 3. 선택된 정점을 경유지로 해서 미방문 정점들로 가는 비용을 따져보고 유리하면 갱신
-            for (int j = 0; j < V; j++) {
-                if (!visited[j] && d[j] > (d[minVertex] + arr[minVertex][j])) {
-                    d[j] = d[minVertex] + arr[minVertex][j]; //갱신
-                }
-            }
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        //출발점을 넣는다.
+        pq.offer(new Node(start,d[start]));
+        while(!pq.isEmpty()) {
+        	//하나 꺼내오기
+        	Node node = pq.poll();
+        	int dist = node.weight; //현재 노드까지의 비용
+        	int now = node.index; //현재 노드
+        	if(d[now]<dist) continue;
+        	for(int i=0;i<graph.get(now).size();i++) {
+        		int cost = d[now]+ graph.get(now).get(i).weight;
+        		//갱신조건
+        		if(cost<d[graph.get(now).get(i).index]) {
+        			d[graph.get(now).get(i).index] = cost;
+        			pq.offer(new Node(graph.get(now).get(i).index,cost));
+        		}
+        	}
         }
-
+        
         for (int i = 0; i < V; i++) {
             if (d[i] == INF) System.out.println("INF");
             else System.out.println(d[i]);
         }
-
-
+        
+        
+        
+	}
+	
+	static class Node implements Comparable<Node>{
+    	int index;
+    	int weight;
+		public Node(int index, int weight) {
+			super();
+			this.index = index;
+			this.weight = weight;
+		}
+		@Override
+		public int compareTo(Node o) {
+			// TODO Auto-generated method stub
+			return this.weight-o.weight;
+		}
+    	
     }
+
 }
---
+++ swea/BJ_18405_경쟁적전염_김건현.java
@@ -0,0 +1,138 @@
+package day_0824;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BJ_18405_경쟁적전염_김건현 {
+	static int N; //배열 범위
+	static int K; //1번부터 K번 바이러스
+	static int S;
+	static int time; //현재 시간
+	static Point[][] area;
+	static Queue<Point> q;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		q = new LinkedList<>();
+		area = new Point[N][N];
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<N;j++) {
+				area[i][j] = new Point(i,j,Integer.parseInt(st.nextToken()),0);
+				if(area[i][j].type>0) {
+					q.offer(area[i][j]);
+				}
+			}
+		}
+		st = new StringTokenizer(br.readLine()); // s,x,y
+		S = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		int Y = Integer.parseInt(st.nextToken());
+		BFS();
+		System.out.println(area[X-1][Y-1].type);
+		
+		
+		
+	}
+	
+	static class Point{
+		int row;
+		int col;
+		int type;
+		int time;
+		public Point(int type, int time) {
+			super();
+			this.type = type;
+			this.time = time;
+		}
+		public Point(int row, int col, int type, int time) {
+			super();
+			this.row = row;
+			this.col = col;
+			this.type = type;
+			this.time = time;
+		}
+		public Point(int row, int col, int type) {
+			super();
+			this.row = row;
+			this.col = col;
+			this.type = type;
+		}
+		
+	}
+	
+	//배열 범위 안인지
+	static boolean isRange(int row, int col) {
+		if(row>=0&&row<N&&col>=0&&col<N) {
+			return true;
+		}
+		return false;
+	}
+	
+	static void print() {
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				System.out.print("|"+area[i][j].type+" "+area[i][j].time+"|");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	//BFS
+	static void BFS() {
+		int[] dx = {-1,1,0,0};
+		int[] dy = {0,0,-1,1};
+		while(!q.isEmpty()) {
+			if(time == S) {
+				break;
+			}
+			int size = q.size();
+			time++;
+			//큐에 있는 해당 레벨만큼 반복
+			for(int i=0;i<size;i++) {
+				//하나 꺼내서
+				Point p = q.poll();
+				if(p.type != area[p.row][p.col].type) {
+					continue;
+				}
+//				p.time = time;
+				//4방향 탐색
+				for(int pos=0;pos<4;pos++) {
+					int x = p.row+dx[pos];
+					int y = p.col+dy[pos];
+					//범위안이면 -> 우선 바이러스 인지 비어있는지.
+					if(isRange(x,y)) {
+						if(area[x][y].type ==0) {
+							area[x][y].type = p.type;
+							area[x][y].time = time;
+							q.offer(new Point(x,y,p.type));
+						}
+						//안이 비어있지 않은 곳. 경쟁 혹은 시간체크
+						else {
+//							if(area[x][y].type == p.type) continue;
+							//시간이 더 에전거이다 -> 이미 고정이니 넘어감 하지만 시간이 0인 0블록은 위에 조건문에서 처리됨
+							if(area[x][y].time!=time) {
+								continue;
+							}
+							//시간이 같다면 -> 경쟁
+							else if(area[x][y].time == time) {
+								if(area[x][y].type > p.type) {
+									area[x][y].type = p.type;
+									q.offer(new Point(x,y,p.type));
+								}
+							}
+						}
+					}
+				}
+			}
+//			print();
+		}
+	}
+}
